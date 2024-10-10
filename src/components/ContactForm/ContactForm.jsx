2fba29ad8242a2576706e4401b375b6d19be3838
Denis2389 <bereznyakdenis2003@gmail.com>
2024-10-10 12:01:38 +0300

    add nanoid

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,31 @@
 import s from './ContactForm.module.css'
-import { Form, Field, Formik } from 'formik'
+import { Form, Field, Formik, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 export default function ContactForm() {
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+    .min(3, 'The name must contain at least 3 characters')
+    .max(50, 'The name cannot exceed 50 characters')
+    .required('Name is required'),
+    number: Yup.string().required('Number is required')
+  })
+
   return (
     <div className={s.wrapper}>
-        <Formik>
+        <Formik
+        validationSchema={validationSchema}
+        >
         <Form className={s.form}>
             <label className={s.label}>
                 <span>Name</span>
-                <Field className={s.field} name="username"/>
+                <Field className={s.field} name="name"/>
             </label>
             <label className={s.label}>
                 <span>Email</span>
-                <Field className={s.field} name="email"/>
+                <Field className={s.field} name="number"/>
             </label>
-            <button>Add contact</button>
+            <button type='submit'>Add contact</button>
         </Form>
         </Formik>
     </div>
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,16 +1,15 @@
 import s from '../ContactForm/ContactForm.module.css'
 
-export default function SearchBox() {
+export const SearchBox = () => {
   return (
     <div className={s.wrapper}> {
             <form className={s.form}>
                 <label className={s.label}>
                     <span>Find contacts by name</span>
-                    <input className={s.field} name="find"/>
+                    <input type='text' placeholder='Find...' className={s.field} name="find"/>
                 </label>
             </form>
     }
-
     </div>
   )
 }
